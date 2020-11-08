0663ec92b7589c0655c1dc3877394a3423c2e316
Zachary Lynch <zachary.lynch1337@gmail.com>
2020-11-08 18:34:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solutions in this file
 function writeCards(names) {
-  x = []
+  let x = []
   for (let i = 0; i <= names.length; i++){
    x[i] = `Thank you, ${names[i]}, for the wonderful surprise gift!`
   }
