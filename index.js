b92b398960436642c0814038ccb35fcbc9e87243
Zachary Lynch <zachary.lynch1337@gmail.com>
2020-11-08 18:31:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@
 function writeCards(names) {
   
   for (int i = 0; i <= names.length; i++)
-  console.log('Thank you, ${array[i]}, for the wonderful surprise gift!')
+  console.log('Thank you, ${names[i]}, for the wonderful surprise gift!')
 }
